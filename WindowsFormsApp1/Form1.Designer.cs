888fca367090eb619176213313e27d76cffa172c
Duwayne Jackson <duwaynejackson754@gmail.com>
2019-02-23 14:29:58 +0200

    Project Name Changed C# Code added to send emails

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,67 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.recipiant = new System.Windows.Forms.TextBox();
+            this.myEmailAddress = new System.Windows.Forms.TextBox();
+            this.body = new System.Windows.Forms.TextBox();
+            this.subject = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.myPassword = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.btnAttachement = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.attachement = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // recipiant
             // 
-            this.textBox1.Location = new System.Drawing.Point(283, 39);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(148, 26);
-            this.textBox1.TabIndex = 0;
+            this.recipiant.Location = new System.Drawing.Point(213, 27);
+            this.recipiant.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.recipiant.Name = "recipiant";
+            this.recipiant.Size = new System.Drawing.Size(218, 26);
+            this.recipiant.TabIndex = 0;
             // 
-            // textBox2
+            // myEmailAddress
             // 
-            this.textBox2.Location = new System.Drawing.Point(283, 423);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(148, 26);
-            this.textBox2.TabIndex = 1;
+            this.myEmailAddress.Location = new System.Drawing.Point(213, 347);
+            this.myEmailAddress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.myEmailAddress.Name = "myEmailAddress";
+            this.myEmailAddress.Size = new System.Drawing.Size(218, 26);
+            this.myEmailAddress.TabIndex = 1;
             // 
-            // textBox3
+            // body
             // 
-            this.textBox3.Location = new System.Drawing.Point(27, 251);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(404, 127);
-            this.textBox3.TabIndex = 2;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.body.Location = new System.Drawing.Point(27, 175);
+            this.body.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.body.Multiline = true;
+            this.body.Name = "body";
+            this.body.Size = new System.Drawing.Size(404, 127);
+            this.body.TabIndex = 2;
+            this.body.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // textBox4
+            // subject
             // 
-            this.textBox4.Location = new System.Drawing.Point(283, 156);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(148, 26);
-            this.textBox4.TabIndex = 3;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(283, 94);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(148, 26);
-            this.textBox5.TabIndex = 4;
+            this.subject.Location = new System.Drawing.Point(213, 85);
+            this.subject.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(218, 26);
+            this.subject.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(36, 39);
+            this.label1.Location = new System.Drawing.Point(36, 27);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 20);
@@ -99,7 +92,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(36, 429);
+            this.label2.Location = new System.Drawing.Point(36, 353);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 20);
@@ -110,72 +103,89 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(36, 208);
+            this.label3.Location = new System.Drawing.Point(36, 132);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(74, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "Message";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(36, 156);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 20);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Body";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(36, 99);
+            this.label5.Location = new System.Drawing.Point(36, 90);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 20);
             this.label5.TabIndex = 9;
             this.label5.Text = "Subject";
             // 
-            // textBox6
+            // myPassword
             // 
-            this.textBox6.Location = new System.Drawing.Point(283, 475);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(148, 26);
-            this.textBox6.TabIndex = 10;
+            this.myPassword.Location = new System.Drawing.Point(213, 399);
+            this.myPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.myPassword.Name = "myPassword";
+            this.myPassword.Size = new System.Drawing.Size(218, 26);
+            this.myPassword.TabIndex = 10;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(36, 485);
+            this.label6.Location = new System.Drawing.Point(36, 409);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(78, 20);
             this.label6.TabIndex = 11;
             this.label6.Text = "Password";
             // 
+            // btnAttachement
+            // 
+            this.btnAttachement.Location = new System.Drawing.Point(27, 463);
+            this.btnAttachement.Name = "btnAttachement";
+            this.btnAttachement.Size = new System.Drawing.Size(120, 36);
+            this.btnAttachement.TabIndex = 12;
+            this.btnAttachement.Text = "Attachement";
+            this.btnAttachement.UseVisualStyleBackColor = true;
+            this.btnAttachement.Click += new System.EventHandler(this.btnAttachement_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(311, 463);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(120, 36);
+            this.btnSend.TabIndex = 13;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // attachement
+            // 
+            this.attachement.Location = new System.Drawing.Point(222, 131);
+            this.attachement.Name = "attachement";
+            this.attachement.Size = new System.Drawing.Size(100, 26);
+            this.attachement.TabIndex = 14;
+            this.attachement.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(456, 524);
+            this.Controls.Add(this.attachement);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnAttachement);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.myPassword);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.body);
+            this.Controls.Add(this.myEmailAddress);
+            this.Controls.Add(this.recipiant);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
@@ -187,18 +197,19 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox recipiant;
+        private System.Windows.Forms.TextBox myEmailAddress;
+        private System.Windows.Forms.TextBox body;
+        private System.Windows.Forms.TextBox subject;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox myPassword;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAttachement;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox attachement;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
 
 namespace WindowsFormsApp1
 {
@@ -24,6 +27,55 @@ namespace WindowsFormsApp1
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                MailMessage message = new MailMessage();                    //  Initialize a new instance of the mailMessage class        
+                message.From = new MailAddress(myEmailAddress.Text);        //Email address from 
+                foreach (string toRecipiant in recipiant.Text.Split(';'))   //Recipiant email address (using the To Add method in the MailMessage class)
+                    message.To.Add(toRecipiant);
+                message.Subject = subject.Text;                             // email subject (using the subject method in the MailMessage class)
+                message.Body = body.Text;                                   //message body (using the body method in the )
+
+                message.Attachments.Add(new Attachment(attachement.Text));  //initializing a new instance of the attachement classs
+
+                //server details
+
+                SmtpClient client = new SmtpClient();
+                client.Credentials = new NetworkCredential(myEmailAddress.Text, myPassword.Text);
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.Send(message);
+            }     
+            catch
+            {
+                MessageBox.Show("An error occured, please see that all the fields are completed correctly", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
+            }
+
+
+
+        }
+
+        private void btnAttachement_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();                      //the openFileDialog will open the dialog allowing you to select a file
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            { 
+               string filePath = ofd.FileName.ToString();                   
+
+                attachement.Text = filePath;                                //the file path will be displayed, but I will make the textbox hidden 
+
+            }
+
+
 
         }
     }
